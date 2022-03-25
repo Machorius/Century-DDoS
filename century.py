e070e675299ca7497f4a6288d23941c727986b61
Ghouler <96554099+Ghoul3r@users.noreply.github.com>
2022-03-25 07:58:44 -0400

    Update century.py

--- century.py
+++ century.py
@@ -7,6 +7,32 @@
 #
 # author : Ghoul3r , version 1.0
 # ----------------------------------------------------------------------------------------------
+"""
+Century - DDoS
+Date    : 25-03-2022
+Author  : Ghoul3r
+Purpose : Century DDoS tool
+
+Copyright (c) 2022 Ghoul3r
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import sys
 import os
 import time
@@ -35,7 +61,6 @@ ip = raw_input("IP Target: ")
 port = input("Port: ")
 
 os.system("clear")
-os.system("figlet Preparing Attack")
 def progressbar(it, prefix="", size=60, file=sys.stdout):
     count = len(it)
 
@@ -63,8 +88,6 @@ for i in progressbar(range(100), "Pinning Server: ", 20):
 os.system('clear')
 for i in progressbar(range(100), "Initializing: ", 20):
     time.sleep(0.01)
-os.system("figlet Century is Underway")
-time.sleep(3)
 os.system('clear')
 sent = 0
 while True:
