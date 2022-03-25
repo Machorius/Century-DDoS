236c1c60409e010725fb1919fb32397bc7ff33ad
Ghouler <96554099+Ghoul3r@users.noreply.github.com>
2022-03-25 08:04:43 -0400

    Update century.py

--- century.py
+++ century.py
@@ -1,38 +1,33 @@
-# ----------------------------------------------------------------------------------------------
-# Century - iDDoS Tool
-#
-# DDoSing is the most massive type of web and server attack
-# This tool can takedown strong webstites including goverment servers as well
-# 
-#
-# author : Ghoul3r , version 1.0
-# ----------------------------------------------------------------------------------------------
-"""
-Century - DDoS
-Date    : 25-03-2022
-Author  : Ghoul3r
-Purpose : Century DDoS tool
+# ----------------------------------------------------------------------------------------------|
+# Century - iDDoS Tool                                                                          |
+#                                                                                               |
+# DDoSing is the most massive type of web and server attack                                     |
+# This tool can takedown strong webstites including goverment servers as well                   |
+#                                                                                               |
+#                                                                                               |
+# author : Ghoul3r , version 1.0                                                                |
+# ----------------------------------------------------------------------------------------------|
+# Copyright (c) 2022 Ghoul3r                                                                    |
+# ----------------------------------------------------------------------------------------------|
+# Permission is hereby granted, free of charge, to any person obtaining a copy                  |
+# of this software and associated documentation files (the "Software"), to deal                 |
+# in the Software without restriction, including without limitation the rights                  |
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                     |
+#copies of the Software, and to permit persons to whom the Software is                          |
+#furnished to do so, subject to the following conditions:                                       |
+#                                                                                               |
+# The above copyright notice and this permission notice shall be included in all                |
+# copies or substantial portions of the Software.                                               |
+# ----------------------------------------------------------------------------------------------|
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                    |
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                      |
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                   |
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                        |
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                 |
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                 |
+# SOFTWARE.                                                                                     |
+# ----------------------------------------------------------------------------------------------|
 
-Copyright (c) 2022 Ghoul3r
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import sys
 import os
 import time
